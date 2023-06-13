89d27baff1998b046254d35f12ed17885693c67f
greffgreff <greffchandler80@gmail.com>
2023-06-13 13:47:13 +0200

    Added user registration tests

--- src/scripts/processor.js
+++ src/scripts/processor.js
@@ -1,14 +1,29 @@
 const fs = require("fs");
-const faker = require("faker");
 const path = require("path");
+
+function generateRandomString(length) {
+  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let randomString = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters[randomIndex];
+  }
+
+  return randomString;
+}
 
 module.exports = {
   generateSignupData: (requestParams, ctx, ee, next) => {
     const filePath = path.join(__dirname, "./material/exchange-keys.json");
     const file = fs.readFileSync(filePath);
-    const displayName = faker.internet.userName().replace("_", "");
-    ctx.vars["displayName"] = displayName;
+    ctx.vars["displayName"] = generateRandomString(10);
     ctx.vars["exchangeKeys"] = JSON.parse(file);
+    return next();
+  },
+  log: console.log,
+  logResponse: (requestParams, response, context, ee, next) => {
+    console.log("Response Body:", response.body);
     return next();
   },
 };
